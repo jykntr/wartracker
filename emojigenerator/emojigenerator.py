63762f3171aa89644f11b0a177db882577ed7ada
jykntr <jykntr@gmail.com>
2018-07-31 22:09:25 -0600

    Updated emojigenerator with clean exit and line printing

--- emojigenerator/emojigenerator.py
+++ emojigenerator/emojigenerator.py
@@ -1,31 +1,46 @@
-import sys
-
 import click
 import discord
 
-client = discord.Client()
 
+class EmojiClient(discord.Client):
+    def __init__(self, output_file):
+        super().__init__()
+        self.output_file = output_file
 
-@client.event
-async def on_ready():
-    with open(client._file_name, "w", newline="\n") as f:
-        print("We have logged in as {0.user}".format(client))
+    async def on_ready(self):
+        print("We have logged in as {0.user}".format(self))
         print("")
-        f.write("emojis = {\n")
-        for guild in client.guilds:
-            f.write("    # Emojis from server '{}'\n".format(guild.name))
-            for emoji in guild.emojis:
-                f.write('    "{1}": "<:{1}:{0}>",\n'.format(emoji.id, emoji.name))
 
-        f.write("}\n")
+        with open(self.output_file, "w", newline="\n") as f:
+            start = "emojis = {\n"
+            f.write(start)
+            print(start)
 
-    sys.exit(0)
+            for guild in self.guilds:
+                name_comment = "    # Emojis from server '{}'\n".format(guild.name)
+                f.write(name_comment)
+                print(name_comment)
+
+                for emoji in guild.emojis:
+                    key_value = '    "{1}": "<:{1}:{0}>",\n'.format(
+                        emoji.id, emoji.name
+                    )
+                    f.write(key_value)
+                    print(key_value)
+
+            end = "}\n"
+            f.write(end)
+            print(end)
+
+            print("Successfully wrote {}.".format(self.output_file))
+
+        self.loop.stop()
 
 
 @click.command()
 @click.option(
     "--bot-token",
-    envvar="BOTTOKEN",
+    envvar="BOT_TOKEN",
     prompt="Enter the discord bot token",
     help="Discord bot token.",
 )
@@ -34,7 +49,7 @@ async def on_ready():
     type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True),
 )
 def cli(bot_token, output_file):
-    client._file_name = output_file
+    client = EmojiClient(output_file)
     client.run(bot_token)
 
 
